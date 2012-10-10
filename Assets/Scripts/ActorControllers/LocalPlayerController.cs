2fff1c9480c47a66cbba2cabe86e9e195d5fb15c
golergka <golergka@gmail.com>
2012-10-10 21:37:26 +0400

    LocalPlayerController — синглтон #2
    Теперь он хранит ссылку на текущего игрока. Под-классам необходимо
    будет вызывать метод Start при переопределении

--- Assets/Scripts/ActorControllers/LocalPlayerController.cs
+++ Assets/Scripts/ActorControllers/LocalPlayerController.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
 using System.Collections;
 
+// serves as a singleton for a localplayer
 public abstract class LocalPlayerController : PlayerController {
+
+	public static LocalPlayerController localPlayer;
+
+	virtual void Start() {
+
+		LocalPlayerController.localPlayer = this;
+
+	}
 
 }
