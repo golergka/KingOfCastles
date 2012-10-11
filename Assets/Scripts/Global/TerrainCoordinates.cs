d27bd0dd5600d6178fa875babf5898ed0462de57
golergka <golergka@gmail.com>
2012-10-11 18:55:36 +0400

    Fixed TerrainCoordinates

--- Assets/Scripts/Global/TerrainCoordinates.cs
+++ Assets/Scripts/Global/TerrainCoordinates.cs
@@ -9,7 +9,7 @@ public class TerrainCoordinates : MonoBehaviour {
 
 	}
 
-	static public Vector3 TerrainToGlobal(Vector2 coordinates, float lift) {
+	static public Vector3 TerrainToGlobal(Vector2 coordinates, float lift = 0f) {
 
 		Vector3 result = new Vector3( coordinates.x, 0, coordinates.y );
 		result.y = Terrain.activeTerrain.SampleHeight(result) + lift;
